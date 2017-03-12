51d3998cd87434eeb5d442756933da1d7532197b
iddiaz <diazdiazivan@gmail.com>
2017-03-12 13:26:37 +0100

    Configuracion de rutas navegacion y animacion basicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
+//core
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//routes
+import { APP_ROUTING } from './app.routes';
+
+//services
+
+//components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -20,7 +27,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
